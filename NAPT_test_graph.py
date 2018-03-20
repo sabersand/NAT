177dd7bd9af049764254ddf5296d12d7c767f8d5
sabersand <32390530+sabersand@users.noreply.github.com>
2018-03-20 18:06:38 +0100

    This is the topology for the controller

---
+++ NAPT_test_graph.py
@@ -0,0 +1,25 @@
+from mininet.topo import Topo
+
+class MyTopo(Topo):
+    "Simple_topology_example"
+    def __init__(self):
+        "Create_custom_topo."
+
+        Topo.__init__(self)
+
+        host1 = self.addHost(name = 'host1',ip = '192.168.0.1/24')
+        host2 = self.addHost(name = 'host2',ip = '192.168.0.2/24')
+        host3 = self.addHost(name = 'host3',ip = '192.168.0.3/24')
+
+        server1 = self.addHost(name = 'server1', ip = '192.168.1.1/24')
+
+        switch1 = self.addSwitch('s1')
+        router1 = self.addSwitch('r2')
+
+        self.addLink(host1, switch1)
+        self.addLink(host2, switch1)
+        self.addLink(host3, switch1)
+        self.addLink(switch1, router1)
+        self.addLink(router1, server1)
+
+topos = {'mytopo':MyTopo}
